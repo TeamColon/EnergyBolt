4dce058337d3ba68891fd87a4c728aacb420b242
pju0205 <pju0205@naver.com>
2025-10-28 17:50:30 +0900

    25.10.28 Create ActorSpawner
    - Actor Spawner 구현

---
+++ Source/EnergyBolt/Private/Actor/EnergySpawner.cpp
@@ -0,0 +1,78 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/EnergySpawner.h"
+
+#include "Character/EnergyBaseCharacter.h"
+#include "Components/ArrowComponent.h"
+
+AEnergySpawner::AEnergySpawner()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	ArrowComponent = CreateDefaultSubobject<UArrowComponent>(TEXT("ArrowComponent"));
+	ArrowComponent->SetupAttachment(RootComponent);
+	ArrowComponent->SetHiddenInGame(true);
+}
+
+void AEnergySpawner::SpawnSelectedActor()
+{
+	if (ActorsArray.IsEmpty()) return;
+	
+	for (int32 i = 0; i < ActorsArray.Num(); i++)
+	{
+		FVector Location = GetActorLocation();
+		FRotator Rotation = FRotator::ZeroRotator;
+		if (AActor* SpawnedActor = GetWorld()->SpawnActor<AActor>(ActorsArray[i], Location, Rotation))
+		{
+			if (AEnergyBaseCharacter* Character = Cast<AEnergyBaseCharacter>(SpawnedActor))
+			{
+				Character->SpawnDefaultController();
+			}
+		}
+		
+		/**
+		 * 몬스터 소환 시 바라보는 방향을 Spawner의 Rotation이 아닌 Target에 대해 바라보게 하려고 했으나
+		 * SetCombatTarget이 BehaviorTree(PossessedBy) 이후 실행되기 때문에 GetCombatTarget이 nullptr 반환되는 것 같다.
+		 */
+		/*FTransform SpawnTransform;
+		SpawnTransform.SetLocation(GetActorLocation());
+		SpawnTransform.SetRotation(FRotator::ZeroRotator.Quaternion());
+		
+		AActor* SpawnedActor = GetWorld()->SpawnActorDeferred<AActor>(
+			ActorsArray[i],
+			SpawnTransform,
+			nullptr,
+			nullptr,
+			ESpawnActorCollisionHandlingMethod::AlwaysSpawn
+		);
+
+		if (!SpawnedActor) return;
+		if (AEnergyBaseCharacter* Character = Cast<AEnergyBaseCharacter>(SpawnedActor))
+		{
+			Character->SpawnDefaultController();
+
+			if (AActor* Target = Character->Execute_GetCombatTarget(Character))
+			{
+				const FRotator CharacterRotation = (GetActorLocation() - Target->GetActorLocation()).Rotation();
+				Character->SetActorRotation(CharacterRotation);
+			}
+			Character->FinishSpawning(SpawnTransform);
+		}
+		else
+		{
+			SpawnedActor->FinishSpawning(SpawnTransform);
+		}*/
+		
+	}
+	
+}
+
+void AEnergySpawner::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
---
+++ Source/EnergyBolt/Public/Actor/EnergySpawner.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interface/CombatInterface.h"
+#include "EnergySpawner.generated.h"
+
+class UArrowComponent;
+
+UCLASS()
+class ENERGYBOLT_API AEnergySpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AEnergySpawner();
+
+	UFUNCTION(BlueprintCallable)
+	void SpawnSelectedActor();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	UArrowComponent* ArrowComponent;
+
+	UPROPERTY(EditAnywhere)
+	TArray<TSubclassOf<AActor>> ActorsArray;
+	
+	UPROPERTY(EditDefaultsOnly)
+	int32 SpawnNum;
+
+
+};
